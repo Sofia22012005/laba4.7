558268f4c32adb5805c760ae4242f1b9d833fa0c
Sofia <kulchitskasofia@gmail.com>
2022-10-27 18:53:52 +0300

    laba4.7

---
+++ laba4.7/laba4.7.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+	double xp, xk, x, dx, eps, a = 0, R = 0, S = 0;
+	int n = 0;
+	const double PI = 3.141592653589793;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "(PI/2)- atan(x)" << " |"
+		<< setw(7) << "(PI/2)+S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		n = 0; 
+		a = -x; 
+		S = a;
+		do {
+			n++;
+			R = ((x*x)-(2*n*x*x))/(2*n+1);
+			a *= R;
+			S += a;
+		} while (abs(a) >= eps);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << (PI / 2) - atan(x) << " |"
+			<< setw(10) << setprecision(5) << (PI / 2) + S << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
